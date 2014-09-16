898682e03f3a5fd860ceef56e00bd0dca9b668ee
Alexandre Gaigalas <alexandre@gaigalas.net>
2014-09-16 11:44:54 -0300

    Improved test/matrix with helper functions

--- test/matrix.sh
+++ test/matrix.sh
@@ -1,3 +1,44 @@
+#
+# Mosai Workshop Test Matrix
+#
+
+# Main tested shells
+env_common_busybox  () ( var TARGET_SHELL="busybox sh" SHELL_PKG="busybox" )
+env_common_dash     () ( var TARGET_SHELL="dash"       SHELL_PKG="dash"    )
+env_common_bash     () ( var TARGET_SHELL="bash"       SHELL_PKG="bash"    )
+env_shell_zsh       () ( var TARGET_SHELL="zsh"        SHELL_PKG="zsh"     )
+env_shell_ksh       () ( var TARGET_SHELL="ksh"        SHELL_PKG="ksh"     )
+env_shell_pdksh     () ( var TARGET_SHELL="pdksh"      SHELL_PKG="pdksh"   )
+env_shell_mksh      () ( var TARGET_SHELL="mksh"       SHELL_PKG="mksh"    )
+
+# Extra shells for Debian-based distros provided by a PPA
+# See the setup_ppa helper function
+env_extras_bash2_0  () ( var TARGET_SHELL="bash2.05b"  SHELL_PKG="bash2.05b"\
+	                     PPA_REQUIRED="agriffis/bashes F5751EC8" )
+env_extras_bash3_0  () ( var TARGET_SHELL="bash3.0.16" SHELL_PKG="bash3.0.16"\
+	                     PPA_REQUIRED="agriffis/bashes F5751EC8" )
+env_extras_bash3_2  () ( var TARGET_SHELL="bash3.2.48" SHELL_PKG="bash3.2.48"\
+	                     PPA_REQUIRED="agriffis/bashes F5751EC8" )
+env_extras_bash4_2  () ( var TARGET_SHELL="bash4.2.45" SHELL_PKG="bash4.2.45"\
+                             PPA_REQUIRED="agriffis/bashes F5751EC8" )
+env_extras_zsh_beta () ( var TARGET_SHELL="zsh-beta"   SHELL_PKG="zsh-beta" )
+
+# Environments for Travis CI
+env_travis_linux () ( var TRAVIS_OS="linux" )
+env_travis_osx   () ( var TRAVIS_OS="osx"   )
+
+# deb/rpm environment machines for vagrant
+env_deb_precise64   () ( var VAGRANT_MACHINE="precise64"  )
+env_deb_trusty64    () ( var VAGRANT_MACHINE="trusty64"   )
+env_deb_lucid64     () ( var VAGRANT_MACHINE="lucid64"    )
+env_deb_debian7464  () ( var VAGRANT_MACHINE="debian7464" )
+env_deb_debian6064  () ( var VAGRANT_MACHINE="debian6064" )
+env_rpm_fedora2064  () ( var VAGRANT_MACHINE="fedora2064" )
+env_rpm_fedora1964  () ( var VAGRANT_MACHINE="fedora1964" )
+env_rpm_centos6464  () ( var VAGRANT_MACHINE="centos6464" )
+env_rpm_centos5     () ( var VAGRANT_MACHINE="centos5"    )
+
+
 # Local Test Matrix
 matrix_local ()
 {
@@ -11,14 +52,14 @@ matrix_local ()
 # Virtual Test Matrix
 matrix_virtual ()
 {
-	setup  () ( vagrant up   $MACHINE )
-	clean  () ( vagrant halt $MACHINE )
+	setup  () ( vagrant up   $VAGRANT_MACHINE )
+	clean  () ( vagrant halt $VAGRANT_MACHINE )
 	script () 
 	{
 		posit_opt=" --shell \"$TARGET_SHELL\" --report tiny"
 		posit_cmd="$TARGET_SHELL bin/posit $posit_opt run test/"
 
-		vagrant ssh $MACHINE -c "cd /vagrant; $posit_cmd"
+		vagrant ssh $VAGRANT_MACHINE -c "cd /vagrant; $posit_cmd"
 	}
 
 	include deb_* common_*
@@ -27,7 +68,7 @@ matrix_virtual ()
 	include rpm_* common_*
 	include rpm_* shell_zsh
 	include rpm_* shell_ksh
-	exclude rpm_centos5 common_busybox_sh
+	exclude rpm_centos5 common_busybox
 	exclude rpm_centos5 common_dash
 }
 
@@ -36,44 +77,19 @@ matrix_travis ()
 {
 	setup ()
 	{
-		cat <<-INFO
+		echo "Setting up build for '$TARGET_SHELL' on '$TRAVIS_OS'..."
 
-			Initializing build for $TARGET_SHELL on $TRAVIS_OS...
+		# No setup if environment has no packages
+		[ -z "$SHELL_PKG" ] && return
 
-		INFO
+		# Install PPA if required
+		[ ! -z "$PPA_REQUIRED" ] && setup_ppa $PPA
 
-		if [ "Linux" = "$(uname -s)" ] && [ ! -z "$SHELL_PKG" ]; then 
+		# Install packages for linux
+		[ "Linux" = "$(uname -s)" ]  && setup_apt "$SHELL_PKG"
 
-			if [ "yes" = "$PPA_REQUIRED" ]; then 
-				cat <<-INFO
-
-					A PPA is required for this environment. Installing...
-
-				INFO
-				sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F5751EC8 | sed 's/^/ >/'
-				sudo bash -c ". /etc/lsb-release; echo deb http://ppa.launchpad.net/agriffis/bashes/ubuntu \$DISTRIB_CODENAME main >> /etc/apt/sources.list"
-			fi
-
-			cat <<-INFO
-
-				Packages from apt are required for this environment. Installing...
-
-			INFO
-			sudo apt-get update -qq | sed 's/^/ >/'
-			sudo apt-get install -y $SHELL_PKG | sed 's/^/ >/'
-		fi
-
-		if [ "Darwin" = "$(uname -s)" ] && [ ! -z "$SHELL_PKG" ]; then 
-
-			cat <<-INFO
-
-				Packages from brew are required for this environment. Installing...
-
-			INFO
-
-			brew update | sed 's/^/ >/'
-			brew install $SHELL_PKG | sed 's/^/ >/'
-		fi
+		# Install packages for OS X
+		[ "Darwin" = "$(uname -s)" ] && setup_brew "$SHELL_PKG"
 	}
 
 	script ()
@@ -91,32 +107,36 @@ matrix_travis ()
 	include travis_osx common_bash
 }
 
-# Environment List
+# Helper function to add a ppa to the apt sources
+setup_ppa ()
+{
+	address="$1"
+	keys="$2"
 
-env_travis_linux () ( var TRAVIS_OS="linux" )
-env_travis_osx   () ( var TRAVIS_OS="osx"   )
+	echo "A PPA is required for this environment. Installing..."
 
-env_deb_precise64     () ( var MACHINE="precise64"  )
-env_deb_trusty64      () ( var MACHINE="trusty64"   )
-env_deb_lucid64       () ( var MACHINE="lucid64"    )
-env_deb_debian7464    () ( var MACHINE="debian7464" )
-env_deb_debian6064    () ( var MACHINE="debian6064" )
-env_rpm_fedora2064    () ( var MACHINE="fedora2064" )
-env_rpm_fedora1964    () ( var MACHINE="fedora1964" )
-env_rpm_centos6464    () ( var MACHINE="centos6464" )
-env_rpm_centos5       () ( var MACHINE="centos5"    )
+	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys "$keys" | sed 's/^/ > /'
+	sudo bash -c ". /etc/lsb-release; echo deb http://ppa.launchpad.net/$address/ubuntu \$DISTRIB_CODENAME main >> /etc/apt/sources.list"
+}
 
-env_common_busybox_sh () ( var TARGET_SHELL="busybox sh" SHELL_PKG="busybox" )
-env_common_dash       () ( var TARGET_SHELL="dash"       SHELL_PKG="dash"    )
-env_common_bash       () ( var TARGET_SHELL="bash"       SHELL_PKG="bash"    )
+# Helper function to install apt packages
+setup_apt ()
+{
+	pkgspec="$1"
 
-env_extras_bash2_05b  () ( var TARGET_SHELL="bash2.05b"  SHELL_PKG="bash2.05b"  PPA_REQUIRED="yes" )
-env_extras_bash3_0_16 () ( var TARGET_SHELL="bash3.0.16" SHELL_PKG="bash3.0.16" PPA_REQUIRED="yes" )
-env_extras_bash3_2_48 () ( var TARGET_SHELL="bash3.2.48" SHELL_PKG="bash3.2.48" PPA_REQUIRED="yes" )
-env_extras_bash4_2_45 () ( var TARGET_SHELL="bash4.2.45" SHELL_PKG="bash4.2.45" PPA_REQUIRED="yes" )
-env_extras_zsh_beta   () ( var TARGET_SHELL="zsh-beta"   SHELL_PKG="zsh-beta" )
+	echo "Packages from apt are required for this environment. Installing..."
 
-env_shell_zsh         () ( var TARGET_SHELL="zsh"   SHELL_PKG="zsh"   )
-env_shell_ksh         () ( var TARGET_SHELL="ksh"   SHELL_PKG="ksh"   )
-env_shell_pdksh       () ( var TARGET_SHELL="pdksh" SHELL_PKG="pdksh" )
-env_shell_mksh        () ( var TARGET_SHELL="mksh"  SHELL_PKG="mksh"  )
+	sudo apt-get update -qq | sed 's/^/ > /'
+	sudo apt-get install -y $pkgspec | sed 's/^/ > /'
+}
+
+# Helper function to install brew packages
+setup_brew ()
+{
+	pkgspec="$1"
+
+	echo "Packages from brew are required for this environment. Installing..."
+
+	brew update | sed 's/^/ > /'
+	brew install $pkgspec | sed 's/^/ > /'
+}
